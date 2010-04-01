ceac1ef6240007a17b07a56ffbb07494fbc22ce6
Dmitry Ivanov <ethercrow@gmail.com>
2010-04-01 18:26:35 +0700

    lower settings

--- unufo_consts.h
+++ unufo_consts.h
@@ -6,7 +6,7 @@
 const int max_neighbours         = 40000;
 
 const int max_threads = 4;
-const bool update_undo_stack         = false;
+const bool update_undo_stack         = true;
 const size_t important_count         = 6;
 
 #endif // ESYNTH_CONSTS_H
